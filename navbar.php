ddbedf0f0ce86508f55f8dc6e2a7aab56684eccd
Sherin Sneha J <67852150+Sherin-Sneha@users.noreply.github.com>
2021-03-10 23:10:25 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,13 +1,22 @@
 <!-- navbar --> 
       <nav class="navbar navbar-expand-md">
-      <a class="navbar-brand" href="index.php">THE BANK OF SPARKS FOUNDATION</a>
+      <a class="navbar-brand" href="index.php">THE SPARKS BANK</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav ml-auto">
+
               <li class="nav-item">
                 <a class="nav-link" href="index.php">HOME</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">VIEW CUSTOMERS</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">TRANSACTION</a>
               </li>
 
               <li class="nav-item">
